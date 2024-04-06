4aa8190b8ec3d4d0cf5b49a38b7796d5be472de3
PhanmaiPhal <phannmai576@gmail.com>
2024-04-06 20:18:46 +0700

    fix size logo

--- src/components/navbar/NavbarComponent.tsx
+++ src/components/navbar/NavbarComponent.tsx
@@ -7,7 +7,7 @@ export default function NavbarComponent() {
             <nav className="bg-white border-gray-200 dark:bg-gray-900 text-[18px]">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <Image src="https://cambodia.mom-gmr.org/uploads/tx_lfrogmom/media/11-400_import.png" className="h-8" alt="Flowbite Logo" width={200} height={200} />
+                        <Image src="https://cambodia.mom-gmr.org/uploads/tx_lfrogmom/media/11-400_import.png" className="h-8" alt="Flowbite Logo" width={50} height={50} />
                         <span className="self-center text-3xl font-semibold whitespace-nowrap dark:text-white">CTN</span>
                     </a>
                     <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
